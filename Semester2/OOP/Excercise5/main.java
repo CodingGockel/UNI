1a0c066fe904a566dc8a9c53d4737126bb7ab2bb
GlatterAal <64667805+GlatterAal@users.noreply.github.com>
2023-05-31 21:23:59 +0200

    hallo

---
+++ Semester2/OOP/Excercise5/main.java
@@ -0,0 +1,17 @@
+interface GraphObj {
+    public void setColor(int x);
+    public void show();
+    public void hide();
+}
+
+interface ResizeableGO {
+    public void setColor(int x);
+    public void show();
+    public void hide();
+}
+
+interface MoveableGO {
+    public void setColor(int x);
+    public void show();
+    public void hide();
+}
